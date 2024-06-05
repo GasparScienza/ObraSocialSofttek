de81940a84db2080605b2fc314feb1de7ec65d97
facubeltran <facundocristianbeltran@gmail.com>
2024-06-05 20:28:26 -0300

    creacion de entidades profMed, especialidad y repo de profmed

---
+++ src/main/java/org/group2/Repository/ProfesionalMedicoRepository.java
@@ -0,0 +1,14 @@
+package org.group2.Repository;
+
+import java.util.List;
+
+import org.group2.Model.ProfesionalMedico;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProfesionalMedicoRepository implements PanacheRepository<ProfesionalMedico>{
+
+	   
+}
